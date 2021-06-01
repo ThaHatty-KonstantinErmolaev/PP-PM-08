e43e9609c6457e5d23ef97c0af59aeb6e7bf2588
Konstantin <kostyaermol@mail.ru>
2021-06-01 12:26:21 +0700

    Added modal window script with hamburger menu animation. Used: https://codepen.io/designcouch/pen/Atyop classes, and https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_modal script. As a result there is my script (combined source's scripts).

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,24 @@
-let menu_item, menu = [];
 
-for (let i = 0; i < 1; i++ ) {
-    menu_item = document.getElementById('menu_item');
+// Get the modal
+let modal = document.getElementById("myModal");
 
-    /*menu = [
-        menu_item = 1,
-    ];*/
+let counter = 0;
+// Get the button that opens the modal
+let button = document.getElementById("openModal");
+
+let hamburger = document.getElementById("nav-icon4");
+
+
+// When the user clicks the button, open the modal
+button.onclick = function() {
+
+    if (counter % 2 === 0) {
+        modal.style.display = "block";
+        counter++;
+        hamburger.classList.add("open");
+    } else {
+        modal.style.display = "none";
+        counter--;
+        hamburger.classList.remove("open");
+    }
 }
